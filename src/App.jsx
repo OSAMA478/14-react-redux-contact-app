b83781066e3085d082fc6d0a459918595010d5ba
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-10-08 19:26:31 +0530

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-	return <h1 className="text-red-500 bg-slate-500">rect-redux</h1>;
+	return (
+		<>
+			<header className="bg-slate-500 text-white text-5xl p-4">
+				<p>Heading</p>
+			</header>
+			<div className="bg-slate-200 h-[calc(100vh-80px)]">all contacts</div>
+		</>
+	);
 }
 
 export default App;
